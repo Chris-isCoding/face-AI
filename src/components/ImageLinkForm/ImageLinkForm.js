9177e0f4139117d9a262daca00cc6d1e625fd564
Chris K <Chris-SourceCode@outlook.com>
2021-08-15 13:57:35 +0100

    refactor[CSS]: Adjust colors for readability

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -4,7 +4,7 @@ import "./ImageLinkForm.css";
 const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div>
-      <p className="f3">
+      <p className="f3 bg-light-blue">
         {"This magic app will detect faces in your pictures. Give it a try."}
       </p>
       <div className="center">
